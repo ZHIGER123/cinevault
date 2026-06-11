80534f10e65f10777c0eafc91717e50bea0200a5
Zhiger <munajtpasziger@gmail.com>
2026-06-11 17:02:21 +0500

    feat: project structure, tailwind config, types

---
+++ stores/admin.ts
@@ -0,0 +1,7 @@
+import { defineStore } from 'pinia'
+
+export const useMyAdminStore = defineStore({
+  id: 'myAdminStore',
+  state: () => ({ }),
+  actions: {}
+})
---
+++ stores/movies.ts
@@ -0,0 +1,7 @@
+import { defineStore } from 'pinia'
+
+export const useMyMoviesStore = defineStore({
+  id: 'myMoviesStore',
+  state: () => ({ }),
+  actions: {}
+})
